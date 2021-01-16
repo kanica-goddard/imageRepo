0ef01076dab0818c5b3272e60e7d7cfa21277fa1
Kanika Sharma <kanica1606gmail.com>
2021-01-16 18:36:31 -0500

    bcrypt

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
-// Entry point file 
+const express = require("express");
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const mongoose = require("mongoose");
+const cors = require('cors');
+const session = require('express-session');
 
-const express = require("express"); 
+//Loads environment variables from the keys.env
+require("dotenv").config({ path: "./config/keys.env" });
 
-const app = express();// create app object by calling the express function
+//App object
+const app = express();
 
-const port = 3000; 
-//set up a server by calling the listen method
-app.listen(PORT,()=>{
-    console.log(`Server is listening`);
+//Middleware 
+app.use(bodyParser.urlencoded({ extended: false })); //bodyparser middleware
+app.use(cors());
+app.use(fileUpload());
+app.use(express.static('public')); // Serve public folder 
+app.use(session({
+    secret: `${process.env.SECRET_KEY}`,
+    resave: false,
+    saveUninitialized: true,
+  }))
+
+
+//Load controllers
+const generalController = require("./controllers/general");
+const imageController = require("./controllers/image");
+
+//Map each controller to the app express object
+app.use("/", generalController);
+app.use("/", imageController);
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+    .then(() => {
+        console.log(`Connected to MongoDB database`);
+    })
+    .catch((err) =>
+        console.log(`Error occured when connecting to database ${err}`)
+    );
+
+const PORT = process.env.PORT;
+//Set up a server by calling the listen method
+app.listen(PORT, () => {
+    console.log(`Your web server has been connected to port ${PORT}.`);
+});
